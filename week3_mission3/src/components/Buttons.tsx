f4432b3e05c682c2ccab8bde9c130b8a2cecca8a
hyunmin1756 <wowangela1756@gmail.com>
2026-04-05 01:23:04 +0900

    week3_mission3_1_23

---
+++ week3_mission3/src/components/Buttons.tsx
@@ -0,0 +1,30 @@
+interface Props {
+    page: number;
+    setPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function Buttons({ page, setPage }: Props) {
+    return (
+        <div className='flex items-center justify-center gap-6 mt-5'>
+            {/*이전 페이지 버튼*/}
+            <button
+                className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#b2dab1] transition-all duration-200 disabled:bg-gray-300 cursor-pointer disabled:cursor-not-allowed'
+                disabled={page === 1} //1페이지에서는 비활성화
+                onClick={() => setPage((prev) => prev - 1)} //이전 페이지
+            >
+                {`<`}
+            </button>
+
+            {/*현재 페이지*/}
+            <span>{page} 페이지</span>
+
+            {/*다음 페이지*/}
+            <button
+                className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer'
+                onClick={() => setPage((prev) => prev + 1)}
+            >
+                {`>`}
+            </button>
+        </div>
+    );
+}
---
+++ week3_mission3/src/pages/NotFoundPage.tsx
@@ -0,0 +1,4 @@
+export default function NotFounPage() {
+    return <div>못찾겠다 꾀꼬리
+    </div>
+}
