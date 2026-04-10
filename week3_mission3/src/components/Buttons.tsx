b644053b096f3073a32a8c255911d2d1710506cd
hyunmin1756 <wowangela1756@gmail.com>
2026-04-10 23:26:57 +0900

    pr리뷰 반영

--- week3_mission3/src/components/Buttons.tsx
+++ week3_mission3/src/components/Buttons.tsx
@@ -1,29 +1,29 @@
-interface Props {
+interface PaginationButtonsProps {
     page: number;
     setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function Buttons({ page, setPage }: Props) {
+export default function PaginationButtons({ page, setPage }: PaginationButtonsProps) {
     return (
         <div className='flex items-center justify-center gap-6 mt-5'>
-            {/*이전 페이지 버튼*/}
+            {/* 이전 페이지 버튼 */}
             <button
                 className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#b2dab1] transition-all duration-200 disabled:bg-gray-300 cursor-pointer disabled:cursor-not-allowed'
-                disabled={page === 1} //1페이지에서는 비활성화
-                onClick={() => setPage((prev) => prev - 1)} //이전 페이지
+                disabled={page === 1}
+                onClick={() => setPage((prev) => prev - 1)}
             >
-                {`<`}
+                {"<"}
             </button>
 
-            {/*현재 페이지*/}
+            {/* 현재 페이지 */}
             <span>{page} 페이지</span>
 
-            {/*다음 페이지*/}
+            {/* 다음 페이지 버튼 */}
             <button
                 className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer'
                 onClick={() => setPage((prev) => prev + 1)}
             >
-                {`>`}
+                {">"}
             </button>
         </div>
     );
