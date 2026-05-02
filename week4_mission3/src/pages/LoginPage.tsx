c52cc67ccc5f6a2774468464cedfdd3765258533
hyunmin1756 <wowangela1756@gmail.com>
2026-05-02 16:54:51 +0900

    week4_mission3be연결

--- week4_mission3/src/pages/LoginPage.tsx
+++ week4_mission3/src/pages/LoginPage.tsx
@@ -1,72 +1,86 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { login } from "../apis/auth";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export default function LoginPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ mode: "onChange" });
 
-    const handleLogin = () => {
-        const savedUser = localStorage.getItem("user");
+  const onSubmit = async (data: FormData) => {
+    try {
+      const res = await login(data.email, data.password);
 
-        //가입된 유저가 아닐 때(localStorage에 없을때)
-        if (!savedUser) {
-            alert("회원가입 먼저 해주세요!");
-            return;
-        }
+      localStorage.setItem("accessToken", res.data.accessToken);
 
-        const parsedUser = JSON.parse(savedUser);
+      alert("로그인 성공!");
+      navigate("/");
+    } catch (e) {
+      alert("로그인 실패");
+    }
+  };
 
-        //이메일+비밀번호 검증
-        if (email === parsedUser.email && password === parsedUser.password) {
-            alert("로그인 성공!");
-            navigate("/");
-        } else {
-            setPassword(""); //틀리면 비밀번호 초기화
-            alert("이메일 또는 비밀번호가 틀렸습니다.");
-        }
-    };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-[350px] bg-white p-6 rounded-2xl shadow-md"
+      >
+        <button onClick={() => navigate(-1)}>{"<"}</button>
+        <h2 className="mb-4">로그인</h2>
 
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-[350px] bg-white p-6 rounded-2xl shadow-md">
-                <div className="flex items-center mb-4">
+<input
+  {...register("email", {
+    required: "이메일을 입력해주세요.",
+    pattern: {
+      value: /\S+@\S+\.\S+/,
+      message: "유효하지 않은 이메일 형식입니다.",
+    },
+  })}
+  placeholder="이메일"
+  className="w-full p-3 border rounded-lg mb-2"
+/>
 
-                    <button
-                        onClick={() => navigate(-1)}
-                        className="p-2 rounded-full hover:bg-gray-200 cursor-pointer transition-all duration-200 active:scale-90"
-                    >
-                        {"<"}
-                    </button>
+{errors.email && (
+  <p className="text-red-500 text-sm mb-2">
+    {errors.email.message}
+  </p>
+)}
 
-                    <h2 className="ml-3 text-lg font-semibold">
-                        로그인
-                    </h2>
+<input
+  type="password"
+  {...register("password", {
+    required: "비밀번호를 입력해주세요.",
+    minLength: {
+      value: 6,
+      message: "비밀번호는 최소 6자 이상이어야 합니다.",
+    },
+  })}
+  placeholder="비밀번호"
+  className="w-full p-3 border rounded-lg mb-2"
+/>
 
-                </div>
-                <input
-                    className="w-full p-3 border rounded-lg mb-2"
-                    placeholder="이메일"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <input
-                    type="password"
-                    className="w-full p-3 border rounded-lg mb-2"
-                    placeholder="비밀번호"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <button
-                    onClick={handleLogin}
-                    className="w-full py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
-                >
-                    로그인
-                </button>
-            </div>
-        </div>
-    );
+{errors.password && (
+  <p className="text-red-500 text-sm mb-2">
+    {errors.password.message}
+  </p>
+)}
+        <button
+          disabled={!isValid}
+          className="w-full bg-blue-500 text-white py-2"
+        >
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 }
--- week4_mission3/src/pages/SignupPage.tsx
+++ week4_mission3/src/pages/SignupPage.tsx
@@ -1,217 +1,185 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { signup } from "../apis/auth";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().email("올바른 이메일 형식을 입력해주세요."),
+  password: z.string().min(6, "비밀번호는 6자 이상이어야 합니다."),
+  confirm: z.string(),
+  nickname: z.string().min(1, "닉네임 입력"),
+}).refine((data) => data.password === data.confirm, {
+  message: "비밀번호가 일치하지 않습니다.",
+  path: ["confirm"],
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function SignupPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [step, setStep] = useState(1);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [step, setStep] = useState(1);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
 
-    const [email, setEmail] = useState("");
-    const [emailTouched, setEmailTouched] = useState(false);
+  const email = watch("email");
 
-    const [password, setPassword] = useState("");
-    const [passwordTouched, setPasswordTouched] = useState(false);
+  const onSubmit = async (data: FormData) => {
+    try {
+      await signup(data.email, data.password, data.nickname);
+      alert("회원가입 완료!");
+      navigate("/login");
+    } catch {
+      alert("회원가입 실패");
+    }
+  };
 
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [confirmTouched, setConfirmTouched] = useState(false);
-
-    const [nickname, setNickname] = useState("");
-
-    const [showPassword, setShowPassword] = useState(false);
-
-    //유효성검사
-    //이메일: @와 .포함 여부 확인
-    const isValidEmail = email.includes("@") && email.includes(".");
-    //비밀번호: 6자 이상
-    const isValidPassword = password.length >= 6;
-    //비밀번호 일치 확인
-    const isMatch = password === confirmPassword;
-
-    //버튼 활성화 조건
-    const canNextStep1 = isValidEmail;
-    const canNextStep2 = isValidPassword && isMatch;
-    const canSubmit = nickname.length > 0;
-
-    //이메일 중복 확인(localStorage와 비교)
-    const checkDuplicateEmail = () => {
-        const savedUser = localStorage.getItem("user");
-
-        if (!savedUser) return false; //저장된 유저 없을 때
-
-        const parsedUser = JSON.parse(savedUser);
-
-        //이메일 동일하면 중복
-        return parsedUser.email === email;
-    };
-
-    //회원가입
-    const handleSignup = () => {
-        const userData = {
-            email,
-            password,
-            nickname,
-        };
-
-        //localStorage에 유저 저장
-        localStorage.setItem("user", JSON.stringify(userData));
-
-        alert("회원가입 완료!");
-        navigate("/"); //홈으로 이동
-    };
-
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-[350px] bg-white p-6 rounded-2xl shadow-md">
-
-                <div className="flex items-center mb-4">
-                    <button
-                        onClick={() => navigate(-1)}
-                        className="p-2 rounded-full hover:bg-gray-200 cursor-pointer transition-all duration-200 active:scale-90"
-                    >
-                        {"<"}
-                    </button>
-                    <h2 className="ml-3 text-lg font-semibold">회원가입</h2>
-                </div>
-
-                {/*step1: 이메일 입력*/}
-                {step === 1 && (
-                    <>
-                        {/*이메일 입력 필드*/}
-                        <input
-                            className={`w-full p-3 border rounded-lg outline-none
-              ${emailTouched && !isValidEmail ? "border-red-500" : "border-gray-300"}`}
-                            placeholder="이메일을 입력해주세요!"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            onBlur={() => setEmailTouched(true)}
-                        />
-
-                        {emailTouched && !isValidEmail && (
-                            <p className="text-red-500 text-sm mt-1">
-                                올바른 이메일 형식을 입력해주세요.
-                            </p>
-                        )}
-
-                        <button
-                            onClick={() => {
-                                if (checkDuplicateEmail()) { //중복된 이메일인지 확인 먼저
-                                    alert("이미 회원가입한 이메일입니다.");
-                                    return;
-                                }
-                                setStep(2);
-                            }}
-                            disabled={!canNextStep1}
-                            className={`w-full mt-4 py-3 rounded-lg transition-colors duration-200
-              ${
-                                canNextStep1
-                                    ? "bg-blue-500 text-white hover:bg-blue-700 cursor-pointer"
-                                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
-                            }`}
-                        >
-                            다음
-                        </button>
-                    </>
-                )}
-
-                {/*step2: 비밀번호 입력*/}
-                {step === 2 && (
-                    <>
-                        {/*입력된 이메일 표시*/}
-                        <p className="mb-2 text-sm text-gray-500">{email}</p>
-
-                        {/*비밀번호 입력*/}
-                        <div className="flex items-center border rounded-lg p-2 mb-2">
-                            <input
-                                type={showPassword ? "text" : "password"} //show상태에 따라
-                                placeholder="비밀번호"
-                                className="flex-1 outline-none"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                onBlur={() => setPasswordTouched(true)}
-                            />
-                            <button
-                                type="button"
-                                onClick={() => setShowPassword((prev) => !prev)}
-                                className="text-gray-500 hover:text-black transition-colors duration-200 hover:scale-110"
-                            >
-                                {showPassword ? "hide" : "👁️"}
-                            </button>
-
-                        </div>
-
-                        {/*비밀번호 길이 확인*/}
-                        {passwordTouched && !isValidPassword && (
-                            <p className="text-red-500 text-sm mb-2">
-                                비밀번호는 6자 이상이어야 합니다.
-                            </p>
-                        )}
-
-                        {/*비밀번호 확인*/}
-                        <input
-                            type="password"
-                            placeholder="비밀번호 확인"
-                            className={`w-full p-3 border rounded-lg mb-2
-              ${confirmTouched && !isMatch ? "border-red-500" : ""}`}
-                            value={confirmPassword}
-                            onChange={(e) =>
-                                setConfirmPassword(e.target.value)
-                            }
-                            onBlur={() => setConfirmTouched(true)}
-                        />
-
-                        {confirmTouched && !isMatch && (
-                            <p className="text-red-500 text-sm">
-                                비밀번호가 일치하지 않습니다.
-                            </p>
-                        )}
-
-                        <button
-                            onClick={() => setStep(3)}
-                            disabled={!canNextStep2}
-                            className={`w-full mt-4 py-3 rounded-lg transition-colors duration-200
-              ${
-                                canNextStep2
-                                    ? "bg-blue-500 text-white hover:bg-blue-700 cursor-pointer"
-                                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
-                            }`}
-                        >
-                            다음
-                        </button>
-                    </>
-                )}
-
-                {/*step3: 닉네임 입력*/}
-                {step === 3 && (
-                    <>
-                        <p className="text-sm text-gray-500 mb-3">
-                            {email}
-                        </p>
-
-                        {/*프로필 이미지 ui만*/}
-                        <div className="w-20 h-20 bg-gray-200 rounded-full mx-auto mb-4" />
-
-                        <input
-                            placeholder="닉네임 입력"
-                            className="w-full p-3 border rounded-lg"
-                            value={nickname}
-                            onChange={(e) => setNickname(e.target.value)}
-                        />
-
-                        <button
-                            onClick={handleSignup}
-                            disabled={!canSubmit}
-                            className={`w-full mt-4 py-3 rounded-lg transition-colors duration-200
-              ${
-                                canSubmit
-                                    ? "bg-blue-500 text-white hover:bg-blue-700 cursor-pointer"
-                                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
-                            }`}
-                        >
-                            회원가입 완료
-                        </button>
-                    </>
-                )}
-            </div>
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-[350px] bg-white p-6 rounded-2xl shadow-md"
+      >
+        <div className="flex items-center mb-4">
+          <button
+            onClick={() => navigate(-1)}
+            className="p-2 rounded-full hover:bg-gray-200 cursor-pointer transition-all duration-200 active:scale-90"
+          >
+            {"<"}
+          </button>
+          <h2 className="ml-3 text-lg font-semibold">회원가입</h2>
         </div>
-    );
+
+        {/* STEP 1 */}
+        {step === 1 && (
+          <>
+            <input
+              {...register("email")}
+              placeholder="이메일을 입력해주세요!"
+              className={`w-full p-3 border rounded-lg mb-2 ${
+                errors.email ? "border-red-500" : ""
+              }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mb-2">
+                {errors.email.message}
+              </p>
+            )}
+
+            <button
+              type="button"
+              onClick={() => setStep(2)}
+              disabled={!!errors.email}
+              className={`w-full mt-2 py-3 rounded-lg ${
+                !errors.email
+                  ? "bg-blue-500 text-white hover:bg-blue-700 cursor-pointer"
+                  : "bg-gray-300 text-gray-500 cursor-not-allowed"
+              }`}
+            >
+              다음
+            </button>
+          </>
+        )}
+
+        {/* STEP 2 */}
+        {step === 2 && (
+          <>
+            <p className="mb-2 text-sm text-gray-500">{email}</p>
+
+            <div className="flex items-center border rounded-lg p-2 mb-2">
+              <input
+                type={showPassword ? "text" : "password"}
+                {...register("password")}
+                placeholder="비밀번호"
+                className="flex-1 outline-none"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                👁️
+              </button>
+            </div>
+
+            {errors.password && (
+              <p className="text-red-500 text-sm mb-2">
+                {errors.password.message}
+              </p>
+            )}
+
+            <input
+              type="password"
+              {...register("confirm")}
+              placeholder="비밀번호 확인"
+              className={`w-full p-3 border rounded-lg mb-2 ${
+                errors.confirm ? "border-red-500" : ""
+              }`}
+            />
+
+            {errors.confirm && (
+              <p className="text-red-500 text-sm">
+                {errors.confirm.message}
+              </p>
+            )}
+
+            <button
+              type="button"
+              onClick={() => setStep(3)}
+              disabled={!!errors.password || !!errors.confirm}
+              className={`w-full mt-2 py-3 rounded-lg ${
+                !errors.password && !errors.confirm
+                  ? "bg-blue-500 text-white hover:bg-blue-700"
+                  : "bg-gray-300 text-gray-500 cursor-not-allowed"
+              }`}
+            >
+              다음
+            </button>
+          </>
+        )}
+
+        {/* STEP 3 */}
+        {step === 3 && (
+          <>
+            <p className="text-sm text-gray-500 mb-3">{email}</p>
+
+            <div className="w-20 h-20 bg-gray-200 rounded-full mx-auto mb-4" />
+
+            <input
+              {...register("nickname")}
+              placeholder="닉네임 입력"
+              className="w-full p-3 border rounded-lg mb-2"
+            />
+
+            {errors.nickname && (
+              <p className="text-red-500 text-sm mb-2">
+                {errors.nickname.message}
+              </p>
+            )}
+
+            <button
+              type="submit"
+              disabled={!isValid}
+              className={`w-full mt-2 py-3 rounded-lg ${
+                isValid
+                  ? "bg-blue-500 text-white hover:bg-blue-700"
+                  : "bg-gray-300 text-gray-500 cursor-not-allowed"
+              }`}
+            >
+              회원가입 완료
+            </button>
+          </>
+        )}
+      </form>
+    </div>
+  );
 }
