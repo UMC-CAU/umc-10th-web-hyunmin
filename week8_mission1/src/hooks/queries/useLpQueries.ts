6f388513ad3e9b35b87f1f5906f8b4a818c18412
hyunmin1756 <wowangela1756@gmail.com>
2026-05-24 01:30:21 +0900

    feat: #20 구현

--- week8_mission1/src/hooks/queries/useLpQueries.ts
+++ week8_mission1/src/hooks/queries/useLpQueries.ts
@@ -1,26 +1,40 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLPList, getLPDetail } from "../../apis/lp";
+import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
 
-//LP 목록 조회
-export const useLPListQuery = (sort: "asc" | "desc") => {
-    return useQuery({
-        queryKey: ["lps", sort],
+import {getLPList, getLPDetail} from "../../apis/lp";
 
-        queryFn: () => getLPList(0, 10, "", sort),
-
-        staleTime: 1000 * 60, //1분
-
-        gcTime: 1000 * 60 * 5, //5분
+// LP 목록 조회
+export const useLPListQuery = (
+    search: string,
+    sort: "asc" | "desc"
+) => {
+    return useInfiniteQuery({
+        queryKey: ["lps", search, sort],
+        queryFn: ({ pageParam = 0 }) =>
+            getLPList(
+                pageParam as number,
+                10,
+                search,
+                sort
+            ),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage: any) => {
+            if (!lastPage.data.hasNext) {
+                return undefined;
+            }
+            return lastPage.data.nextCursor;
+        },
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 10,
     });
 };
 
-//LP 상세 조회
-export const useLPDetailQuery = (lpId: string) => {
+// LP 상세 조회
+export const useLPDetailQuery = (
+    lpId: string
+) => {
     return useQuery({
         queryKey: ["lp", lpId],
-
         queryFn: () => getLPDetail(lpId),
-
         enabled: !!lpId,
     });
 };
