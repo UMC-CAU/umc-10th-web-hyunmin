ba87c236155f19d4e7cd6f5214f9941b51f4f1b1
hyunmin1756 <wowangela1756@gmail.com>
2026-05-24 04:30:17 +0900

    feat: #29 구현

--- week8_mission3/src/hooks/useSidebar.ts
+++ week8_mission3/src/hooks/useSidebar.ts
@@ -1,24 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useSidebar() {
-    // Sidebar 열림 상태
-    const [isOpen, setIsOpen] =
-        useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     // 열기
-    const open = () => {
-        setIsOpen(true);
-    };
+    const open = () => setIsOpen(true);
 
     // 닫기
-    const close = () => {
-        setIsOpen(false);
-    };
+    const close = () => setIsOpen(false);
 
     // 토글
     const toggle = () => {
         setIsOpen((prev) => !prev);
     };
+
+    // ESC 키 감지
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                close();
+            }
+        };
+
+        // Sidebar 열려있을 때만 등록
+        if (isOpen) {
+            window.addEventListener(
+                "keydown",
+                handleKeyDown
+            );
+        }
+
+        // cleanup 함수
+        return () => {
+            window.removeEventListener(
+                "keydown",
+                handleKeyDown
+            );
+        };
+    }, [isOpen]);
 
     return {
         isOpen,
